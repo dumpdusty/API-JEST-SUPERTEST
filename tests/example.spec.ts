e23115539d7e7d133965d80d4ab3e32af8aa79b7
Fazil M <dumpdusty@gmail.com>
2024-04-26 19:53:09 -0700

    add diff test & upd yml

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,10 @@
-describe('simple math functions',() =>{
-    it('verify sum', () =>{
-        expect(4+4).toEqual(8)
+describe('simple math functions', () => {
+    it('verify sum', () => {
+        expect(4 + 4).toEqual(8)
+    })
+
+    it('verify diff', () => {
+        expect(4 - 4).toEqual(0)
     })
 
 })
